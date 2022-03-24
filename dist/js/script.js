9b89b8068f47452fd323412e8503f0cfc9748e12
Krzysztof <krzysiekjach@gmail.com>
2022-03-24 21:00:45 +0100

    js for main page

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,101 @@
 "use strict";
 //GLOBAL SCOPE ELEMENTS
-const skills = (document.querySelector('.skills'));
+const skills = document.querySelector('.skills');
+const projects = document.querySelector('.projects');
 //mouseenter
 //mouseleave
 //# MIN SECTIONS
 //## SKILLS
-const text = skills.firstElementChild;
-const img = skills.lastElementChild;
+const skillsText = document.querySelector('.skills__text');
+const skillsImg = document.querySelector('.skills__img');
 skills.addEventListener('mouseenter', function () {
-    img.style.transform = 'scale(0.87) translateY(-12%) rotate(2000deg)';
-    text.style.opacity = '1';
-    text.style.transform = 'none';
+    skillsImg.style.transform = 'scale(0.87) translateY(-12%) rotate(2000deg)';
+    skillsText.style.opacity = '1';
+    skillsText.style.transform = 'none';
     setTimeout(() => {
         skills.addEventListener('click', () => {
-            img.style.display = 'none';
+            skillsImg.style.display = 'none';
         });
     }, 5000);
 });
 skills.addEventListener('mouseleave', function () {
-    img.style.transform = 'none';
-    text.style.opacity = '0';
-    text.style.transform = 'translateY(100%)';
+    skillsImg.style.transform = 'none';
+    skillsText.style.opacity = '0';
+    skillsText.style.transform = 'translateY(100%)';
 });
-// skills.addEventListener('click', function () {
-//   img.style.display = 'none';
-// });
+//##PROJECTS
+const projectsText = document.querySelector('.projects__text');
+const projectsImgGear = document.querySelector('.projects__img-gear');
+projects.addEventListener('mouseenter', function () {
+    projectsImgGear.style.transform = 'rotate(6000deg)';
+    projectsText.style.opacity = '1';
+    setTimeout(() => {
+        projects.addEventListener('click', () => {
+            projectsImgGear.style.display = 'none';
+        });
+    }, 5000);
+});
+projects.addEventListener('mouseleave', function () {
+    projectsImgGear.style.transform = 'none';
+    projectsText.style.opacity = '0';
+});
+//##ROADMAP
+const roadmap = document.querySelector('.roadmap');
+const roadmapFace = document.querySelector('.roadmap__animation__road__part1-face');
+const roadmapTitle = document.querySelector('.roadmap__title');
+const roadmapStart = document.querySelector('.roadmap__animation__start');
+const roadmapFinish = document.querySelector('.roadmap__animation__finish');
+const roadmapLine = document.querySelector('.roadmap__animation__road__part2');
+const roadmapOpacityElements = [
+    roadmapTitle,
+    roadmapStart,
+    roadmapFinish,
+    roadmapLine,
+];
+let roadmapOpacTime;
+const roadmapTimer = function (type) {
+    if (type === 1) {
+        roadmapOpacTime = setTimeout(() => {
+            roadmapFace.setAttribute('src', 'img/road-face2.svg');
+            roadmapFace.parentNode.style.transform =
+                'translateX(90%)';
+            roadmapFace.parentNode.style.transition = 'all 100s';
+        }, 5000);
+    }
+    else {
+        clearTimeout(roadmapOpacTime);
+    }
+};
+let roadmapFaceTime;
+const roadmapFaceTimer = function (type) {
+    if (type === 1) {
+        roadmapFaceTime = setTimeout(() => {
+            roadmapFace.setAttribute('src', 'img/road-face1.svg');
+        }, 5000);
+    }
+    else {
+        clearTimeout(roadmapFaceTime);
+    }
+};
+roadmap.addEventListener('mouseenter', () => {
+    roadmapFace.setAttribute('src', 'img/road-face1.svg');
+    roadmapOpacityElements.forEach(el => {
+        el.style.opacity = '1';
+    });
+    roadmapTimer(1);
+    roadmapFace.style.height = '6rem';
+    roadmapFace.parentNode.style.transform = 'none';
+    roadmapFaceTimer(2);
+});
+roadmap.addEventListener('mouseleave', () => {
+    roadmapTimer(2);
+    roadmapOpacityElements.forEach(el => {
+        el.style.opacity = '0';
+    });
+    roadmapFace.style.height = '20rem';
+    roadmapFace.setAttribute('src', 'img/road-face3.svg');
+    roadmapFace.parentNode.style.transform =
+        'translateX(35%)';
+    roadmapFace.parentNode.style.transition = 'all 5s';
+    roadmapFaceTimer(1);
+});
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,33 +1,121 @@
 //GLOBAL SCOPE ELEMENTS
 
-const skills = (<HTMLElement>(
-  document.querySelector('.skills')
-)) as HTMLDivElement;
-
+const skills = document.querySelector('.skills') as HTMLDivElement;
+const projects = document.querySelector('.projects') as HTMLDivElement;
 //mouseenter
 //mouseleave
 //# MIN SECTIONS
 
 //## SKILLS
-const text = skills.firstElementChild as HTMLParagraphElement;
-const img = skills.lastElementChild as HTMLImageElement;
+const skillsText = document.querySelector(
+  '.skills__text'
+) as HTMLParagraphElement;
+const skillsImg = document.querySelector('.skills__img') as HTMLImageElement;
 
 skills.addEventListener('mouseenter', function () {
-  img.style.transform = 'scale(0.87) translateY(-12%) rotate(2000deg)';
-  text.style.opacity = '1';
-  text.style.transform = 'none';
+  skillsImg.style.transform = 'scale(0.87) translateY(-12%) rotate(2000deg)';
+  skillsText.style.opacity = '1';
+  skillsText.style.transform = 'none';
   setTimeout(() => {
     skills.addEventListener('click', () => {
-      img.style.display = 'none';
+      skillsImg.style.display = 'none';
     });
   }, 5000);
 });
 
 skills.addEventListener('mouseleave', function () {
-  img.style.transform = 'none';
-  text.style.opacity = '0';
-  text.style.transform = 'translateY(100%)';
+  skillsImg.style.transform = 'none';
+  skillsText.style.opacity = '0';
+  skillsText.style.transform = 'translateY(100%)';
 });
-// skills.addEventListener('click', function () {
-//   img.style.display = 'none';
-// });
+
+//##PROJECTS
+const projectsText = document.querySelector(
+  '.projects__text'
+) as HTMLParagraphElement;
+const projectsImgGear = document.querySelector(
+  '.projects__img-gear'
+) as HTMLImageElement;
+projects.addEventListener('mouseenter', function () {
+  projectsImgGear.style.transform = 'rotate(6000deg)';
+  projectsText.style.opacity = '1';
+
+  setTimeout(() => {
+    projects.addEventListener('click', () => {
+      projectsImgGear.style.display = 'none';
+    });
+  }, 5000);
+});
+projects.addEventListener('mouseleave', function () {
+  projectsImgGear.style.transform = 'none';
+  projectsText.style.opacity = '0';
+});
+
+//##ROADMAP
+const roadmap = document.querySelector('.roadmap') as HTMLDivElement;
+const roadmapFace = document.querySelector(
+  '.roadmap__animation__road__part1-face'
+) as HTMLImageElement;
+const roadmapTitle = document.querySelector(
+  '.roadmap__title'
+) as HTMLParagraphElement;
+const roadmapStart = document.querySelector(
+  '.roadmap__animation__start'
+) as HTMLParagraphElement;
+const roadmapFinish = document.querySelector(
+  '.roadmap__animation__finish'
+) as HTMLParagraphElement;
+const roadmapLine = document.querySelector(
+  '.roadmap__animation__road__part2'
+) as HTMLDivElement;
+const roadmapOpacityElements = [
+  roadmapTitle,
+  roadmapStart,
+  roadmapFinish,
+  roadmapLine,
+];
+let roadmapOpacTime: number;
+const roadmapTimer = function (type: 1 | 2) {
+  if (type === 1) {
+    roadmapOpacTime = setTimeout(() => {
+      roadmapFace.setAttribute('src', 'img/road-face2.svg');
+      (roadmapFace.parentNode as HTMLDivElement).style.transform =
+        'translateX(90%)';
+      (roadmapFace.parentNode as HTMLDivElement).style.transition = 'all 100s';
+    }, 5000);
+  } else {
+    clearTimeout(roadmapOpacTime);
+  }
+};
+let roadmapFaceTime: number;
+const roadmapFaceTimer = function (type: 1 | 2) {
+  if (type === 1) {
+    roadmapFaceTime = setTimeout(() => {
+      roadmapFace.setAttribute('src', 'img/road-face1.svg');
+    }, 5000);
+  } else {
+    clearTimeout(roadmapFaceTime);
+  }
+};
+roadmap.addEventListener('mouseenter', () => {
+  roadmapFace.setAttribute('src', 'img/road-face1.svg');
+  roadmapOpacityElements.forEach(el => {
+    el.style.opacity = '1';
+  });
+  roadmapTimer(1);
+  roadmapFace.style.height = '6rem';
+  (roadmapFace.parentNode as HTMLDivElement).style.transform = 'none';
+  roadmapFaceTimer(2);
+});
+roadmap.addEventListener('mouseleave', () => {
+  roadmapTimer(2);
+  roadmapOpacityElements.forEach(el => {
+    el.style.opacity = '0';
+  });
+  roadmapFace.style.height = '20rem';
+  roadmapFace.setAttribute('src', 'img/road-face3.svg');
+  (roadmapFace.parentNode as HTMLDivElement).style.transform =
+    'translateX(35%)';
+  (roadmapFace.parentNode as HTMLDivElement).style.transition = 'all 5s';
+  roadmapFaceTimer(1);
+});
