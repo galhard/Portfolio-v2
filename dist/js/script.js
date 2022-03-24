491f7b6da315947f475a7eea2090a26e4c570569
Krzysztof <krzysiekjach@gmail.com>
2022-03-24 16:10:30 +0100

    f

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,12 +11,17 @@ skills.addEventListener('mouseenter', function () {
     img.style.transform = 'scale(0.87) translateY(-12%) rotate(2000deg)';
     text.style.opacity = '1';
     text.style.transform = 'none';
+    setTimeout(() => {
+        skills.addEventListener('click', () => {
+            img.style.display = 'none';
+        });
+    }, 5000);
 });
 skills.addEventListener('mouseleave', function () {
     img.style.transform = 'none';
     text.style.opacity = '0';
     text.style.transform = 'translateY(100%)';
 });
-skills.addEventListener('click', function () {
-    img.style.display = 'none';
-});
+// skills.addEventListener('click', function () {
+//   img.style.display = 'none';
+// });
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -16,6 +16,11 @@ skills.addEventListener('mouseenter', function () {
   img.style.transform = 'scale(0.87) translateY(-12%) rotate(2000deg)';
   text.style.opacity = '1';
   text.style.transform = 'none';
+  setTimeout(() => {
+    skills.addEventListener('click', () => {
+      img.style.display = 'none';
+    });
+  }, 5000);
 });
 
 skills.addEventListener('mouseleave', function () {
@@ -23,6 +28,6 @@ skills.addEventListener('mouseleave', function () {
   text.style.opacity = '0';
   text.style.transform = 'translateY(100%)';
 });
-skills.addEventListener('click', function () {
-  img.style.display = 'none';
-});
+// skills.addEventListener('click', function () {
+//   img.style.display = 'none';
+// });
